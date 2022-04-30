11142b62b0dd2da0d45459e076ed86a82a22aed6
Zebedeu <marciozebedeu@gmail.com>
2022-04-30 20:56:51 +0100

    update

--- src/index.js
+++ src/index.js
@@ -63,11 +63,10 @@ router.post('/', (req, res) => {
      // DONE!!!
     })
 
-    
+
     app.use('/.netlify/functions/server', router);  // path must route to lambda
-    app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
     
-    
+
     module.exports = app;
     module.exports.handler = serverless(app);
     
